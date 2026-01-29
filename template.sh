1b7b1c95c3be61289cf94778c66e0d883e7c59f1
jatin-agrawal17 <mittaljatin441@gmail.com>
2026-01-30 02:06:58 +0530

    Web Complete

--- template.sh
+++ template.sh
@@ -7,7 +7,7 @@ touch src/prompt.py
 touch .env
 touch setup.py
 touch app.py
-touch research/trials.py
+touch research/trials.ipynb
 touch requirements.txt
 
 echo "Directory structure created successfully."
